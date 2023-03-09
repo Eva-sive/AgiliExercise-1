1493ee339af74a781b5520493c8acc720ef08102
Eva <eva.caroline.szeremeta@gmail.com>
2023-03-09 13:11:35 +0100

    ajout const

--- assets/main.js
+++ assets/main.js
@@ -3,10 +3,10 @@
  * et n'oublie pas de lier ta feuille JavaScript avec ta feuille HTML
 **/
 
-// Commence par déclarer les variabls necessaires
-
+// Commence par déclarer les variables necessaires
+const menuGlobal = document.querySelector('.sidebar_menu')
+const menuContent = document.querySelector('.sidebar')
 
 
 // Rédige les conditions ou fonctions juste ici, n'oublie pas le résultat attendu dans le README.md
-
 
